1b5081f7020b7e911a59ac1a0ada2796846f98a9
Tam Dang <danhtam0509@gmail.com>
2024-09-11 22:56:40 -0400

    added logic to redirect if user login, if there is org, added a variant to the button, added User Button to the navbar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,7 +1,7 @@
 import { Plus } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
-import { OrganizationSwitcher } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 
 export const Navbar = () => {
     return (
@@ -12,11 +12,11 @@ export const Navbar = () => {
                     TrelloTD
                 </div>
 
-                <Button size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+                <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
                     Create
                 </Button>
 
-                <Button size="sm" className="rounded-sm block md:hidden">
+                <Button variant="primary" size="sm" className="rounded-sm block md:hidden">
                     <Plus className="h-4 w-4"/>
                 </Button>
             </div>
@@ -35,6 +35,17 @@ export const Navbar = () => {
                                 alignItems: "center",
                             },
                         },
+                    }}
+                />
+
+                <UserButton
+                    appearance={{
+                        elements: {
+                            avatarBox: {
+                                height: 30,
+                                width: 30,
+                            }
+                        }
                     }}
                 />
             </div>
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher, redirectToSignIn } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
@@ -10,6 +11,29 @@ export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth().protect();
   }
+
+  if (auth().userId && isPublicRoute(request)) {
+    let path = "/select-org";
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+
+    const orgSelection = new URL(path, request.url);
+    return NextResponse.redirect(orgSelection)
+
+  }
+
+  if (!auth().userId && !isPublicRoute(request)) {
+    return auth().redirectToSignIn({ returnBackUrl: request.url })
+  }
+
+  if (auth().userId && !auth().orgId && request.nextUrl.pathname !== "/select-org") {
+    const orgSelection = new URL("/select-org", request.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+
 });
 
 export const config = {
