43b095713d412a6de496bec1d44c575ce6eb09a2
Tam Dang <danhtam0509@gmail.com>
2024-09-30 22:54:55 -0400

    added tooltip hint component

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,4 +1,5 @@
-import { User2 } from "lucide-react"
+import { Hint } from "@/components/hint"
+import { HelpCircle, User2 } from "lucide-react"
 
 export const BoardList = () => {
     return (
@@ -14,6 +15,17 @@ export const BoardList = () => {
                     className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
                 >
                     <p className="text-sm">Create new board</p>
+
+                    <Hint
+                        sideOffset={40}
+                        description={`
+                            Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.    
+                        `}
+                    >
+                        <HelpCircle
+                            className="absolute bottom-2 right-2 h-[14px] w-[14px]"
+                        />
+                    </Hint>
                 </div>
             </div>
         </div>
