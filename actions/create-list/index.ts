3642991de20379494fe8d5db61b98b34d874440e
Tam Dang <danhtam0509@gmail.com>
2024-10-09 00:27:56 -0400

    created List Wrapper component, worked on List Form component, dynamically switching between input and button, can use escape or click outside of element to disable editing, added create list server action, first fetch board by boardId if board still exists, the get list to find last order, then insert new list with new order, then get lists again and return list data, used this new server action in the List Form component

---
+++ actions/create-list/index.ts
@@ -0,0 +1,63 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { InputType, ReturnType } from "./types";
+import db from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { CreateList } from "./schema";
+
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+
+    const { userId, orgId } = auth();
+
+    if (!userId || !orgId) {
+        return {
+            error: "Unauthorized",
+        }
+    }
+
+    const { title, boardId } = data;
+
+    const connection = await db();
+
+    let q = `SELECT * FROM board WHERE id = ? AND orgId = ?`
+
+    const [boards] = await connection.query(q, [boardId, orgId])
+
+    if (!boards[0]) {
+        return {
+            error: "Board not found",
+        }
+    }
+
+    q = `SELECT \`order\` FROM list WHERE boardId = ? ORDER BY 'order' DESC`
+    let [lists] = await connection.query(q, [boardId])
+
+    console.log(lists)
+    const newOrder = lists[0] ? lists[0].order + 1 : 1;
+
+    try {
+        q = `INSERT INTO list (title, boardId, \`order\`) VALUES (?, ?, ?)`
+
+        await connection.query(q, [title, boardId, newOrder])
+    
+    } catch (e) {
+        console.log(e)
+    }
+
+
+    q = `SELECT * FROM list WHERE boardId = ?`;
+    [lists] = await connection.query(q, [boardId])
+
+    // console.log(boards[0]);
+
+    revalidatePath(`/board/${boardId}`)
+
+    return {
+        data: lists[lists.length - 1]
+    }
+}
+
+export const createList = createSafeAction(CreateList, handler)
